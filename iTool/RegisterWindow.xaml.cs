4c75fef9e0d17d9ced3b94be2c5260a3a93ff949
M3156 <m3156@student.jamk.fi>
2019-03-25 15:21:00 +0200

    register

--- iTool/RegisterWindow.xaml.cs
+++ iTool/RegisterWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -63,10 +66,87 @@ namespace iTool
 
         private void BtnReg_Click(object sender, RoutedEventArgs e)
         {
-            users.Add(new User() { UserID = 1, Email = txtAddEmail.Text, Password = pwdCreatePassword.Password, FirstName = txtFirstName.Text, LastName = txtLastName.Text, Mobile = int.Parse(txtMobile.Text), Address = txtAddAddress.Text, Location = txtAddLocation.Text, PaymentMethod = cbPayment.SelectedValue.ToString(), PictureURL = txtPic.Text.Split('\\')[txtPic.Text.Split('\\').Length - 1] });
-            ShowUser user = new ShowUser();
-            user.Show();
-            user.dgUsers.ItemsSource = users;
-        }
+            if (txtAddEmail.Text.Length == 0)
+            {
+                txbError.Text = "Enter an email.";
+                txtAddEmail.Focus();
+            }
+            else if (!Regex.IsMatch(txtAddEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                txbError.Text = "Enter a valid email.";
+                txtAddEmail.Focus();
+            }
+            else if (txtFirstName.Text.Length == 0)
+            {
+                txbError.Text = "Enter your first name";
+                txtFirstName.Focus();
+            }
+            else if (txtLastName.Text.Length == 0)
+            {
+                txbError.Text = "Enter your last name";
+                txtLastName.Focus();
+            }
+            else if (txtMobile.Text.Length < 10)
+            {
+                txbError.Text = "Enter valid mobile number";
+                txtMobile.Focus();
+            }
+            else if (txtAddLocation.Text.Length == 0)
+            {
+                txbError.Text = "Enter your location";
+                txtAddLocation.Focus();
+            }
+            else if (cbPayment == null)
+            {
+                txbError.Text = "Choose the payment method";
+                cbPayment.Focus();
+            }
+            else if (txtAddAddress.Text.Length == 0)
+            {
+                txbError.Text = "Enter your address";
+                txtAddAddress.Focus();
+            }
+            else
+            {
+                string firstname = txtFirstName.Text;
+                string lastname = txtLastName.Text;
+                string email = txtAddEmail.Text;
+                string password = pwdCreatePassword.Password;
+                string location = txtAddLocation.Text;
+                int mobile = int.Parse(txtMobile.Text);
+                string address = txtAddAddress.Text;
+                if (pwdCreatePassword.Password.Length == 0)
+                {
+                    txbError.Text = "Enter password.";
+                    pwdCreatePassword.Focus();
+                }
+                else if (pwdConfirm.Password.Length == 0)
+                {
+                    txbError.Text = "Enter Confirm password.";
+                    pwdConfirm.Focus();
+                }
+                else if (pwdCreatePassword.Password != pwdConfirm.Password)
+                {
+                    txbError.Text = "Confirm password must be same as password.";
+                    pwdConfirm.Focus();
+                }
+                else
+                {
+                    txbError.Text = "";
+                    
+                    SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("Insert into Registration (FirstName,LastName,Email,Password,Address) values('" + firstname + "','" + lastname + "','" + email + "','" + password + "','" + address + "')", con);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    txbError.Text = "You have Registered successfully.";
+                    Reset();
+                }
+                //users.Add(new User() { UserID = 1, Email = txtAddEmail.Text, Password = pwdCreatePassword.Password, FirstName = txtFirstName.Text, LastName = txtLastName.Text, Mobile = int.Parse(txtMobile.Text), Address = txtAddAddress.Text, Location = txtAddLocation.Text, PaymentMethod = cbPayment.SelectedValue.ToString(), PictureURL = txtPic.Text.Split('\\')[txtPic.Text.Split('\\').Length - 1] });
+                //ShowUser user = new ShowUser();
+                //user.Show();
+                //user.dgUsers.ItemsSource = users;
+            }
     }
 }
